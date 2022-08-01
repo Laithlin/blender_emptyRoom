4e9c416d6433946ffddcb8b7c150b03f5e22ad61
Laithlin <justynasikorska15@gmail.com>
2022-08-01 11:52:26 +0200

    change masks

--- create_masks.py
+++ create_masks.py
@@ -2,11 +2,11 @@ import os
 import cv2 as cv
 import sys
 import numpy as np
-from skimage.metrics import structural_similarity as compare_ssim
+# from skimage.metrics import structural_similarity as compare_ssim
 
 
 def create_mask(imgPath1, imgPath2):
-    masksPath = '/home/justyna/All/magisterka/images_models/clear_images/train_data/masks/'
+    masksPath = '/home/justyna/studia/magisterka/images_models/clear_images/train_data/masks/'
     # img1 = cv.imread(imgPath1, 0)
     # img2 = cv.imread(imgPath2, 0)
     # gray_image1 = cv.cvtColor(img1, cv.COLOR_HSV2RGB)
@@ -31,9 +31,26 @@ def create_mask(imgPath1, imgPath2):
 
         cv.imwrite(masksPath + name, img2 - img1)
 
+def change_masks(imgPath):
+    for name in os.listdir(imgPath):
+        img = cv.imread(imgPath + name)
+        ret, thresh = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+
+        cv.imwrite(imgPath + name, thresh)
+
+def check_image(imgPath):
+    img = cv.imread(imgPath)
+    print(img)
+
+    cv.imshow("Mask", img)
+    k = cv.waitKey(0)
 
 if __name__ == '__main__':
-    imgPat1 = '/home/justyna/All/magisterka/images_models/clear_images/train_data/labels/'
-    imgPat2 = '/home/justyna/All/magisterka/images_models/clear_images/train_data/images/'
+    imgPat1 = '/home/justyna/studia/magisterka/images_models/clear_images/train_data/labels/'
+    imgPat2 = '/home/justyna/studia/magisterka/images_models/clear_images/train_data/images/'
+    masksPath = '/home/justyna/studia/magisterka/images_models/clear_images/train_data/masks/'
+    imgPath = '/home/justyna/studia/magisterka/images_models/clear_images/test_data/masks/depth_bathroom_30_0_0_0_0.png'
 
-    create_mask(imgPat1, imgPat2)
+    # check_image(imgPath)
+    # create_mask(imgPat1, imgPat2)
+    change_masks(masksPath)
